1f547a9b5cb5fb5f29b3f1007c378506d663e195
Gfry1234 <iamgfry@yahoo.com>
2022-10-19 22:36:42 -0700

    Write a function that checks for a digit (0 through 9).

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,17 @@
-
 #include "main.h"
 
 /**
- * _isdigit - checks if parameter is a number between 0 to 9.
- * @c: input number.
- * Return: 1 if is a number (0 to 9), 0 in other case.
+ * _isupper - define if a character is uppercase
+ *
+ * Description: Prints the alphabet with _putchar
+ * @c: charater
+ * Return: void
  */
-int _isdigit(int c)
+
+int _isupper(int c)
 {
-	if (c >= 48 && c <= 57)
-	{
+	if (c >= 'A' && c <= 'Z')
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
